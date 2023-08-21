affea02efda149df0a9ff80dcf4ab157696888c9
Lukas Willms <lukas.willms@study.thws.de>
2023-08-21 22:46:16 +0200

    First style test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { TypeAnimation } from 'react-type-animation';
+import CircleWithImage from './Jsx-Components/CircleWithImage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='first-Div'>
+        <a className='einloggen'>Einloggen</a>
+        <img className='menue' src='https://www.svgrepo.com/show/520763/hamburger.svg'></img>
+          <img className="logo" src="https://firebasestorage.googleapis.com/v0/b/movieapp-test-83d87.appspot.com/o/Logo3.png?alt=media&token=84657e46-c9a9-4d6d-913c-518848acf254"></img>
+          <CircleWithImage></CircleWithImage>
+        <div className='Überschrift-Div'>
+          <a className='Überschrift'>Mache jetzt </a>
+          <br></br>
+          <TypeAnimation className='Überschrift_Animiert'sequence={['Kostenlos', 1000, 'Leicht verständlich',1000,'Qualifiziert',1000]} wrapper="span" speed={40} repeat={Infinity} cursor={true}/>
+          <br></br>
+       
+          
+        </div>
+
+    </div>
+
     </div>
   );
 }
---
+++ my-app/src/Jsx-Components/CircleWithImage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const CircleWithImage = () => {
+  return (
+    <div className="circle-container">
+      <div className="circle">
+        <img src="https://images.unsplash.com/photo-1622556498246-755f44ca76f3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80" alt="Bild im Kreis" className="circle-image" />
+      </div>
+    </div>
+  );
+};
+
+export default CircleWithImage;
