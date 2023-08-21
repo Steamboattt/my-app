c7ae0bd9d018c9c14cba9275d0ac356ec1329b9b
Lukas Willms <lukas.willms@study.thws.de>
2023-08-22 00:50:41 +0200

    Style 0.01

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,25 +3,52 @@ import './App.css';
 import { TypeAnimation } from 'react-type-animation';
 import CircleWithImage from './Jsx-Components/CircleWithImage';
 
+
 function App() {
+
+
   return (
     <div className="App">
       <div className='first-Div'>
         <a className='einloggen'>Einloggen</a>
         <img className='menue' src='https://www.svgrepo.com/show/520763/hamburger.svg'></img>
-          <img className="logo" src="https://firebasestorage.googleapis.com/v0/b/movieapp-test-83d87.appspot.com/o/Logo3.png?alt=media&token=84657e46-c9a9-4d6d-913c-518848acf254"></img>
-          <CircleWithImage></CircleWithImage>
+        <img className="logo" src="https://firebasestorage.googleapis.com/v0/b/movieapp-test-83d87.appspot.com/o/Logo3.png?alt=media&token=84657e46-c9a9-4d6d-913c-518848acf254"></img>
+       
+
+        <CircleWithImage></CircleWithImage>
+  
+
         <div className='Überschrift-Div'>
           <a className='Überschrift'>Mache jetzt </a>
           <br></br>
-          <TypeAnimation className='Überschrift_Animiert'sequence={['Kostenlos', 1000, 'Leicht verständlich',1000,'Qualifiziert',1000]} wrapper="span" speed={40} repeat={Infinity} cursor={true}/>
+          <TypeAnimation className='Überschrift_Animiert'sequence={['Kostenlos', 1000,'ohne Anmeldung',1000, 'Leicht verständlich',1000,'Qualifiziert',1000]} wrapper="span" speed={40} repeat={Infinity} cursor={true}/>
           <br></br>
-       
-          
+          <a className='Überschrift'>dein Internet-Führerschein </a>
         </div>
 
     </div>
 
+    <div className='Angebote-Div'>
+      <h1 className='Kategorien'>Kategorien</h1>
+      <div className='Angebot-Div'>
+        <a>Kostenlose Variante</a>
+        <button>Jetzt starten</button>
+      </div>
+
+      <div className='Angebot-Div'>
+        <a>Kostenpflichtige Variante</a>
+        <button>Jetzt starten</button>
+      </div>
+      <div className='Erklärungs-Div'>
+        <h1 className='Kategorien'>Was ist das überhaupt?</h1>
+
+      </div>
+
+    </div>
+
+    <div className='Impressum-Div'>
+      <a>Impressum</a>
+    </div>
     </div>
   );
 }
--- my-app/src/Jsx-Components/CircleWithImage.jsx
+++ my-app/src/Jsx-Components/CircleWithImage.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-
+import { motion, useTransform, useViewportScroll } from "framer-motion"
 
 const CircleWithImage = () => {
+  const { scrollYProgress } = useViewportScroll();
+  
+  // Verwende useTransform, um den scrollYProgress in eine Wertebereich umzuwandeln, den wir verwenden möchten
+  const translateY = useTransform(scrollYProgress, [0, 1], [0, -100]);
+
   return (
     <div className="circle-container">
       <div className="circle">
-        <img src="https://images.unsplash.com/photo-1622556498246-755f44ca76f3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80" alt="Bild im Kreis" className="circle-image" />
+        <motion.img 
+          style={{ translateY }} 
+          src="https://images.unsplash.com/photo-1622556498246-755f44ca76f3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
+          alt="Bild im Kreis"
+          className="circle-image"
+        />
       </div>
     </div>
   );
