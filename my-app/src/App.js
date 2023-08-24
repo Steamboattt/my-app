0c7de6a26d6ccdcf33aa63e46377037bb8678318
Lukas Willms <lukas.willms@study.thws.de>
2023-08-24 17:10:42 +0200

    Style changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,43 @@
 
 import './App.css';
 import { TypeAnimation } from 'react-type-animation';
-import CircleWithImage from './Jsx-Components/CircleWithImage';
+import Lottie from "lottie-react";
+import nachdenklichAnimation from "./Json/man-thinking-something.json";
+import me from "./Json/animation_llp8xi8a.json"
 import {motion,useInView, useAnimation,variants} from "framer-motion";
-import { useEffect, useRef } from 'react';
-
+import { useEffect, useRef , useState} from 'react';
+import {BasisCurve} from "react-svg-curve";
+import CircleWithImage from './Jsx-Components/CircleWithImage';
+import salesman from "./Json/business-salesman.json";
 
 function App() {
 const mainControls = useAnimation();
+const slideControls = useAnimation();
 const ref = useRef(null);
-const isInView = useInView(ref, {once:true})
-
+const isInView = useInView(ref, {once:false})
+const data = [
+  [0, 50],
+  [50, 75],
+  [75, 80],
+  [100, 40],
+  [125, 30],
+  [150, 60],
+  [175, 50]
+];
 useEffect(() =>{
   if (isInView){
     mainControls.start("visible");
-  
+    slideControls.start("visible");
   }
 },[isInView]);
 
 
   return (
     <div className="App">
+   
       <div className='first-Div'>
         <a className='einloggen'>Einloggen</a>
+        
         <img className='menue' src='https://www.svgrepo.com/show/520763/hamburger.svg'></img>
         <img className="logo" src="https://firebasestorage.googleapis.com/v0/b/movieapp-test-83d87.appspot.com/o/Logo3.png?alt=media&token=84657e46-c9a9-4d6d-913c-518848acf254"></img>
        
@@ -39,7 +54,11 @@ useEffect(() =>{
         </div>
 
     </div>
+    <div className='untergrund-Div'>
 
+    </div>
+
+  
     <div className='Angebote-Div'>
 
 
@@ -82,18 +101,32 @@ useEffect(() =>{
         <button className='button-87'>Jetzt starten</button>
       </div>
       <div ref={ref}  className='Erklärungs-Div'>
+
+
         <motion.div variants={{hidden:{opacity: 0, y:75}, visible: {opacity: 1, y:0},}}
         initial="hidden"
         animate={mainControls}
         transition={{duration:0.5, delay: 0.25}}
         >
-                  <h1 className='Kategorien'>Was ist das überhaupt?</h1>
 
-          <a>TEST</a>
+      
+
+
+                  <h1 className='Kategorien'>Was ist das überhaupt?</h1>
+                  <div style={{ display: 'flex', alignItems: 'center', width:"90%" }}>
+                    <Lottie style={{ width: '20%' }} animationData={salesman} />
+                    <p style={{ marginLeft: '10px', textAlign: 'center', flex: 1 }}>Unsere Webseite bietet eine umfassende Ressource zum Thema Internet-Führerschein, konzipiert insbesondere für neue Internetnutzer, darunter ältere Erwachsene und Kinder. Unser Hauptziel ist es, Ihnen dabei zu helfen, die essenziellen Grundkenntnisse über die sichere Nutzung des Internets zu erlernen. In der heutigen digitalen Welt ist es von entscheidender Bedeutung, sich der potenziellen Gefahren bewusst zu sein und zu wissen, wie man sich davor schützen kann. Unsere gut strukturierten Lektionen vermitteln auf verständliche Weise, wie Sie Ihre persönlichen Daten schützen, vertrauenswürdige Online-Quellen identifizieren und sich vor betrügerischen Aktivitäten im Internet absichern können. Ihre neu erworbenen Kenntnisse können Sie in einem abschließenden Test unter Beweis stellen, der Ihnen die Möglichkeit gibt, Ihr Verständnis zu überprüfen. Bei erfolgreichem Abschluss des Tests erhalten Sie ein Zertifikat, das Ihre Bemühungen um die Entwicklung eines sicheren und sachkundigen Umgangs mit dem Internet würdigt. Wir laden Sie ein, unsere Webseite zu erkunden und Ihre Reise zum Internet-Führerschein zu beginnen.</p>
+                  </div>
+
 
         </motion.div>
-        <h1 className='Kategorien'>Über uns</h1>
 
+
+        <h1 className='Kategorien'>Über mich</h1>
+                  <div style={{ display: 'flex', alignItems: 'center',}}>
+                    <Lottie style={{ width: '20%' }} animationData={me} />
+                    <p style={{ marginLeft: '10px', textAlign: 'center', flex: 1 }}>TestText</p>
+                  </div>
       </div>
 
     </div>
